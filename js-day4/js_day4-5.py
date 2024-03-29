17f7f23cb2330eadde6e33bf79703aa44588cf54
Chohy0209 <hisoo1112@gmail.com>
2024-03-29 17:52:04 +0900

    js_day4

---
+++ js-day4/js_day4-5.py
@@ -0,0 +1,17 @@
+# import math
+# b,h=map(int,input("밑변과 높이를 알려주세요").split())
+# print(math.sqrt(b**2+h**2))
+
+#
+# cardinal=input("수를 입력하세요")
+# if int(cardinal)<=0:
+#     print(cardinal)
+# elif cardinal[-1]=='1':
+#     print(cardinal+'st')
+# elif cardinal[-1]=='2':
+#     print(cardinal+'nd')
+# elif cardinal[-1]=='3':
+#     print(cardinal+'rd')
+# else:
+#     print(cardinal+'th')
+
